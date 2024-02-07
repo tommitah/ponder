639a7abefecf4059fd3a786e4e7a6ffc593908a8
tommitah <reijotahvanainen15@gmail.com>
2024-02-07 19:55:29 +0200

    test for pat

--- bin/main.ml
+++ bin/main.ml
@@ -3,5 +3,6 @@
 let () =
   let body = RestClient.fetch_body "http://localhost:3000" in
   (* print out body, which is type string t *)
+  (* this is just a comment*)
   Printf.printf "%s" (Lwt_main.run body)
 ;;
